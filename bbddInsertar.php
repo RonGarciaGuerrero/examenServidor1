649caa5c525e02e4f849912d8f7224ad48b24443
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2021-11-16 20:01:38 +0100

    ejercicio 1 casi terminado

--- bbddInsertar.php
+++ bbddInsertar.php
@@ -1,5 +1,52 @@
 <!-- Autor: Ronald Garcia Guerrero -->
 <?php 
+    //conexion
+    $conn = mysqli_connect("localhost","root","","lindavista");
+    // Comprobar conexion
+    if ($conn->connect_error) {
+        die("Conexion fallida: ".$conn->connect_error);
+    
+    }else{
+        echo "conectado a la BBDD"."<br/>";
+    }
+    //para generar el id he puesto en mysql admin al campo id le he dado a cambiar y he seleccionado el check de auto incremento AI, en el script de sql le he puesto el auto incremento despues del tipo
 
+    // set parameters and execute
+    $tipo = $_POST['tipo'];
+    $zona = $_POST['zona'];
+    $direccion = $_POST['direccion'];
+    $dormitorios = $_POST['dormitorios'];
+    $precio = $_POST['precio'];
+    $tamanio = $_POST['tamanio'];
+    $extras = '';
+    
+    if(isset($_POST['extras'])){    
+        foreach($_POST['extras'] as $value){
+            $extras.=$value.',';        
+        }
+    }
+
+    $foto = $_POST['foto'];
+    $observaciones = $_POST['observaciones'];
+
+    $sql = "INSERT INTO VIVIENDAS (TIPO,ZONA,DIRECCION,DORMITORIOS,PRECIO,TAMANIO,EXTRAS,FOTO,OBSERVACIONES) VALUES ('$tipo','$zona','$direccion','$dormitorios','$precio','$tamanio','$extras','$foto','$observaciones')"; //"insert into viviendas ..."
+
+    if (mysqli_query ($conn, $sql)){
+    echo "Guardado correctamente";
+    }else{
+    echo "Error guardando: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    echo "Los valores guardados son: ";
+    echo "Tipo: ".$tipo. "<br>";
+    echo "Zona: ".$zona. "<br>";
+    echo "Dirección: ".$direccion. "<br>";
+    echo "Dormitorios: ".$dormitorios. "<br>";
+    echo "Precio: ".$precio.'€'. "<br>";
+    echo "Tamaño: ".$tamanio. "<br>";
+    echo "Extras: ".$extras. "<br>";
+    echo "Foto: ".$foto . "<br>";
+    echo "Observaciones: ".$observaciones. "<br>";
+
+    mysqli_close($conn);
 
 ?>
--- insertar.php
+++ insertar.php
@@ -12,32 +12,60 @@
 <h1>Insertar en la BBDD lindavista:</h1><br/>
     <form action="bbddInsertar.php" method="post">
         <label for="tipo">Tipo de vivienda</label><br/>
-        <select name="TIPO" id="tipo">
+        <select name="tipo" id="tipo">
             <option value="PISO">PISO</option>
             <option value="ADOSADO">ADOSADO</option>
             <option value="CHALET">CHALET</option>
             <option value="CASA">CASA</option>
         </select><br/>
         <label for="zona">Zona</label><br/>
-        <input style="text-transform: uppercase;" required type="text" name="zona"><br/>
+        <select name="zona" id="zona">
+            <option value="CENTRO">CENTRO</option>
+            <option value="NERVION">NERVION</option>
+            <option value="TRIANA">TRIANA</option>
+            <option value="ALJARAFE">ALJARAFE</option>
+        </select><br/>
+        
         <label for="direccion">Dirección</label><br/>
         <input style="text-transform: uppercase;" required type="text" name="direccion"><br/>
         <label for="dormitorios">Número de dormitorios</label><br/>
         <select name="dormitorios" id="dormitorios">
             <option value="1">1</option>
             <option value="2">2</option>
-            <option value="3">3</option>
+            <option value="3" selected>3</option>
             <option value="4">4</option>
             <option value="5">5</option>
         </select><br/>
         
-        <label for="cp">Precio</label><br/>
-        <input style="text-transform: uppercase;" required type="text" name="cp"><br/>
-        <label for="telefono">Telefono</label><br/>
-        <input required type="number" name="telefono"><br/>
-        <label for="email">Email</label><br/>
-        <input required type="text" name="email"><br/><br/>
+        <label for="precio">Precio en euros</label><br/>
+        <input required type="number" name="precio" min="1"><br/>
+        <label for="tamanio">Tamaño en m2</label><br/>
+        <input required type="number" name="tamanio" min="1"><br/>
+
+        <label for="extras">Extras</label><br/>
+        <input type="checkbox" id="extra0" name="extras[]" value="N/A">
+        <label for="extra0">No aplica</label><br>
+        <input type="checkbox" id="extra1" name="extras[]" value="PISCINA">
+        <label for="extra1">Piscina</label><br>
+        <input type="checkbox" id="extra2" name="extras[]" value="JARDIN">
+        <label for="extra2">Jardin</label><br>
+        <input type="checkbox" id="extra3" name="extras[]" value="GARAJE">
+        <label for="extra3">Garaje</label><br><br>
+
+        <label for="foto">URL Foto</label><br/>
+        <input type="text" name="foto"><br/><br/>
+        <label for="email">Observaciones</label><br/>
+        <input type="text" name="observaciones"><br/><br/>
+        
+        
+        
         <input type="submit">
     </form>
 </body>
 </html>
+
+<!-- Pendiente:
+controlar las excepciones, posibles errores en los campos 
+lo de sql injection ?
+antonio me dice que escoja un menu desplegable de escoger mas d euna opcion html para evitar que escojan no aplica y piscina 
+-->
